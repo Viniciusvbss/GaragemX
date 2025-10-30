c8b98717c810d329b669c6d0f0fce7b83115bfe5
Vinicius Barbosa Soares da Silva <145233994+Viniciusvbss@users.noreply.github.com>
2025-10-30 09:19:19 -0300

    Criada a pasta src/pages/AuthLojista/ Movido CadastroLoja.jsx para dentro dela Atualizado o import em AppRoutes.jsx:3

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home.jsx';
-import CadastroLoja from '../pages/CadastroLoja.jsx';
+import CadastroLoja from '../pages/AuthLojista/CadastroLoja.jsx';
 
 function AppRoutes() {
   return (
