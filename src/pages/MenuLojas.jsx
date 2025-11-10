0bf0ee58913fdde3add0ed0e886bef23c54eb63c
Vinicius Barbosa Soares da Silva <145233994+Viniciusvbss@users.noreply.github.com>
2025-11-10 10:02:07 -0300

    alteração

--- src/pages/MenuLojas.jsx
+++ src/pages/MenuLojas.jsx
@@ -3,8 +3,21 @@ import BebidasIcon from '../assets/icons/Bebidas.svg?react'
 
 export default function MenuLojas() {
 
+    const servicos = [
+        { nome: 'Bebidas', icone: BebidasIcon },
+        { nome: 'Comidas', icone: BebidasIcon },
+        { nome: 'Sobremesas', icone: BebidasIcon },
+        { nome: 'Lanches', icone: BebidasIcon },
+        { nome: 'Doces', icone: BebidasIcon },
+        { nome: 'Salgados', icone: BebidasIcon },
+        { nome: 'Outros', icone: BebidasIcon },
+        { nome: 'Extras', icone: BebidasIcon },
+        { nome: 'qualquercoisa', icone: BebidasIcon },
+    
+    ]
     const circleclass = [
         "h-20 w-20 rounded-full cursor-pointer",
+        "bg-white/10 active:bg-white/20 hover:bg-white/30",
         "border border-yellow-500 active:border-blue-500 hover:border-red-500",
         "text-yellow-500 active:text-blue-500 hover:text-red-500",
     ].join(" ")
@@ -14,33 +27,11 @@ export default function MenuLojas() {
                 <div className="w-96 h-96 backdrop-blur-sm rounded-xl bg-white/10">
                     <p className="text-white text-2xl font-bold italic text-center p-8 pb-10">Escolha seu serviço</p>
                     <div className="grid grid-cols-3 gap-4">
-                        <div className="flex justify-center items-center bg-transparent">
-                            <BebidasIcon className={circleclass} />
-                        </div>
-                        <div className="flex justify-center items-center bg-transparent">
-                            <BebidasIcon className={circleclass} />
-                        </div>
-                        <div className="flex justify-center items-center bg-transparent">
-                            <BebidasIcon className={circleclass} />
-                        </div>
-                        <div className="flex justify-center items-center bg-transparent">
-                            <BebidasIcon className={circleclass} />
-                        </div>
-                        <div className="flex justify-center items-center bg-transparent">
-                            <BebidasIcon className={circleclass} />
-                        </div>
-                        <div className="flex justify-center items-center bg-transparent">
-                            <BebidasIcon className={circleclass} />
-                        </div>
-                        <div className="flex justify-center items-center bg-transparent">
-                            <BebidasIcon className={circleclass} />
-                        </div>
-                        <div className="flex justify-center items-center bg-transparent">
-                            <BebidasIcon className={circleclass} />
-                        </div>
-                        <div className="flex justify-center items-center bg-transparent">
-                            <BebidasIcon className={circleclass} />
-                        </div>
+                        {servicos.map((servico, index) => (    
+                            <div key={index} className="flex justify-center items-center bg-transparent">
+                                <servico.icone className={circleclass} />
+                            </div>  
+                        ))}
                     </div>
                 </div>
             </div>
